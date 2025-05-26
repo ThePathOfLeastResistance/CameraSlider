24eae2c14352120f90351088efcc9a54398f1d4d
James Cao <127159112+ThePathOfLeastResistance@users.noreply.github.com>
2025-05-25 20:15:52 -0400

    Finished V1 Untested

--- main.ino
+++ main.ino
@@ -1,46 +1,122 @@
 
+//Libs
 #include <Wire.h>
-
 #include <AccelStepper.h>
 
-AccelStepper stepper1(1, 2, 5); // (Typeof driver: with 2 pins, STEP, DIR)
-AccelStepper stepper2(1, 3, 6);
+AccelStepper stepper1(1, 2, 3); // (Typeof driver: with 2 pins, STEP, DIR)
+AccelStepper stepper2(1, 4, 5);
+AccelStepper stepper2(1, 6, 7);
+
+int FStatePin = 12;
+int BStatePin = 11;
+int LimitSwitchPin = 10;
+
+
+#define VRX_PIN  A1
+#define VRY_PIN  A0
+
+int M1SpeedPin = A2;
+int M2SpeedPin = A3;
+int M3SpeedPin = A4;
+
 
 int buttonFState = 0;
 int buttonBState = 0;
 
+int button1MState = 0;
+int button2MState = 0;
+int button3MState = 0;
 
-
-#define VRX_PIN  A1 // Arduino pin connected to VRX pin
-#define VRY_PIN  A0 // Arduino pin connected to VRY pin
 
 int xValue = 0; // To store value of the X axis
 int yValue = 0; // To store value of the Y axis
 
+
+
+
+//https://docs.arduino.cc/built-in-examples/digital/InputPullupSerial/
+
 void setup() {
-  pinMode(13, INPUT);
-  pinMode(12, INPUT);
+  pinMode(FStatePin, INPUT_PULLUP);
+  pinMode(BStatePin, INPUT_PULLUP);
+  pinMode(CalbriationPin, INPUT_PULLUP);
+    
   Serial.begin(9600);
 
-  stepper1.setMaxSpeed(1000); // Set maximum speed value for the stepper
-  stepper1.setAcceleration(500); // Set acceleration value for the stepper
-  stepper1.setCurrentPosition(0); // Set the current position to 0 steps
+  stepper1.setMaxSpeed(1023); 
+  stepper1.setAcceleration(500);
+  stepper1.setCurrentPosition(0);
 
-  stepper2.setMaxSpeed(1000);
+  stepper2.setMaxSpeed(1023);
   stepper2.setAcceleration(500);
   stepper2.setCurrentPosition(0);
-  
+
+  stepper3.setMaxSpeed(1023);
+  stepper3.setAcceleration(500);
+  stepper3.setCurrentPosition(0);
+
+  while(digitalRead(LimitSwitchPin) == 1){
+    stepper1.setSpeed(1000);
+    stepper1.runSpeed();
+    stepper1.setCurrentPosition(0);
+  }
+
+  stepper1.moveTo(-200);
+  stepper1.runToPosition(); 
+
+// Make The LED Green
 }
 
 void loop() {
+
   buttonFState = digitalRead(13);
   buttonBState = digitalRead(12);
-//  Serial.println(buttonFState);
+
+  //  Serial.println(buttonFState);
 //  Serial.println(buttonBState);
   int xValue1 = analogRead(A1);
-  int yValue1 = analogRead(A0);
-  int xValue2 = analogRead(A2);
+//  int yValue1 = analogRead(A0);
+//  int xValue2 = analogRead(A2);
   int yValue2 = analogRead(A3);
+
+  
+  if(buttonFState == 0 && buttonFState == 0){
+    stepper1.setSpeed(0);
+   } else if (buttonFState == 0 && buttonFState != 0) {
+      stepper1.setSpeed(digitalRead(M1SpeedPin));
+    } else if (buttonFState != 0 && buttonFState == 0) {
+      stepper1.setSpeed(-digitalRead(M1SpeedPin));
+    } else {
+         stepper1.setSpeed(0);
+      }
+
+
+
+//FInd the abs value of the thing
+// FInd the values that it is returning
+  if( xValue1 - 500 < 20 || xValue1 < 20) {
+    stepper2.setSpeed(0);
+  } else {
+    stepper2.setSpeed( digitalRead(M2SpeedPin) *   (xValue1 - 500)/1000  )
+    } 
+
+  if( xValue1 - 500 < 20 || xValue1 < 20) {
+    stepper2.setSpeed(0);
+  } else {
+    stepper2.setSpeed( digitalRead(M2SpeedPin) *   (xValue1 - 500)/1000  )
+    } 
+
+  if( yValue1 - 500 < 20 || yValue1 < 20) {
+    stepper3.setSpeed(0);
+  } else {
+    stepper3.setSpeed( digitalRead(M2SpeedPin) *   (xValue1 - 500)/1000  )
+    } 
+
+  stepper1.runSpeed();
+  stepper2.runSpeed();
+  stepper3.runSpeed();
+  
+  
   
   Serial.print("x = ");
   Serial.print(xValue1);
@@ -49,19 +125,4 @@ void loop() {
   delay(200);
 
   
-   stepper1.moveTo(800); // Set desired move: 800 steps (in quater-step resolution that's one rotation)
-  stepper1.runToPosition(); // Moves the motor to target position w/ acceleration/ deceleration and it blocks until is in position
-
-  stepper2.moveTo(1600);
-  stepper2.runToPosition();
-
-  // Move back to position 0, using run() which is non-blocking - both motors will move at the same time
-  stepper1.moveTo(0);
-  stepper2.moveTo(0);
-  while (stepper1.currentPosition() != 0 || stepper2.currentPosition() != 0) {
-    stepper1.run();  // Move or step the motor implementing accelerations and decelerations to achieve the target position. Non-blocking function
-    stepper2.run();
-    //
-    //
-  }
 }
